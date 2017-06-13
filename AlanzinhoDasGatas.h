333a137681bf8f20745f6871d376a2336a878ae1
Júnior <Babilonia364@users.noreply.github.com>
2017-06-12 21:39:23 -0300

    Update AlanzinhoDasGatas.h

--- AlanzinhoDasGatas.h
+++ AlanzinhoDasGatas.h
@@ -1,10 +1,27 @@
 #include <stdio.h>
-int menu();
+#include <stdlib.h>
 int SomaVet(int *V1, int *V2, int n);
-void DeclaraVet();
-int smenu_vetor();
-int smenu_matriz();
+void DeclaraVet(int opc_smenu_vetor);
+void menu();
+int LerVet(int *V1, int k);
 
+struct Vetores
+{
+	int *Vet;
+};
+
+//=== Ler Vetores ===// FEITO POR DasGatas
+
+int LerVet(int *V1, int k) //Tá bugado, tá puxando o menu 2 vezes, REUPI
+{
+	printf("| ");
+	for (int i = 0; i<k; i++)
+	{
+		printf("%d ", V1[i]);
+	}
+	printf("|\n\n");
+	menu();
+}
 
 //=== Soma Vetorial ===// FEITO POR DasGatas
 
@@ -16,172 +33,40 @@ int SomaVet(int *V1, int *V2, int n)
 		Soma[i] = V1[i]+V2[i];
 		printf("%d ", Soma[i]);
 	}
-	printf("\n \n");
+	printf("\n\n");
+	menu();
 }
 
 //=== Declaração de vetores ===// FEITO POR DasGatas
-void DeclaraVet()
+void DeclaraVet(int opc_smenu_vetor)
 {
-	int k;
+	int k, len, Swi;
+	printf("Digite quantos vetores voce quer operar:\n");
+	scanf("%d", &len);
+	struct Vetores V[len];
 	printf("Digite o tamanho dos vetores\n");
 	scanf("%d", &k);
-	int Vet1[k], Vet2[k];
-	for(int i=0; i<k; i++)
+	for(int j=0; j<len; j++)
 	{
-		printf("Digite o %d numero do vetor%d\nDigite o %d numero do vetor%d\n", i+1, 1, i+1, 2);
-		scanf("%d %d", &Vet1[i], &Vet2[i]);
+		V[j].Vet = malloc(k*sizeof(int));
+		for(int i=0; i<k; i++)
+		{
+			printf("Digite o elemento %d do vetor %d: ", i+1, j+1);
+			scanf("%d", &V[j].Vet[i]);
+			printf("\n");
+		}
 	}
-	SomaVet(Vet1, Vet2, k);
-}
-
-//=== Submenu dos Vetores ===// FEITO POR SILVEIRAX
-
-int smenu_vetor () 
-{
-
-    // Area de Declaracao de Variaveis:
-
-    int opc_smenu_vetor;
-
-    // Area do Algoritmo:
-
-    printf("==================================================\n");
-    printf("||             OPERACOES  VETORIAIS             ||\n");
-    printf("==================================================\n");
-    printf("  (1) Ler vetores.\n");
-    printf("  (2) Adicao vetorial.\n");
-    printf("  (3) Subtracao vetorial.\n");
-    printf("  (4) Produto escalar.\n");
-    printf("  (5) Produto vetorial.\n");
-    printf("  (6) Voltar ao Menu Principal.\n");
-    printf("  (7) Sair.\n");
-    printf("      Qual operacao deseja executar? ");
-    scanf("%d", &opc_smenu_vetor);
-
-    switch (opc_smenu_vetor) 
-	{
-        case 1:
-            printf("\n");
-            break;
-        case 2:
-            printf("\n");
-			void DeclaraVet();
-            break;
-        case 3:
-            printf("\n");
-            break;
-        case 4:
-            printf("\n");
-            break;
-        case 5:
-            printf("\n");
-            break;
-        case 6:
-            menu();
-            break;
-        case 7:
-            printf("      Obrigado por utilizar este programa!\n");
-            break;
-        default:
-            printf("      Opcao invalida. Digite outra opcao.\n");
-            smenu_vetor();
-    }
-}
-
-//=== Submenu das Matrizes ===// FEITO POR SILVEIRAX
-
-int smenu_matriz () 
-{
-
-    // Area de Declaracao de Variaveis:
-
-    int opc_smenu_matriz;
-
-    // Area do Algoritmo:
-
-    printf("==================================================\n");
-    printf("||             OPERACOES MATRICIAIS             ||\n");
-    printf("==================================================\n");
-    printf("  (1) Ler matrizes.\n");
-    printf("  (2) Verificador de matrizes especiais.\n");
-    printf("  (3) Adicao matricial.\n");
-    printf("  (4) Subtracao matricial.\n");
-    printf("  (5) Produto matricial.\n");
-    printf("  (6) Divisao matricial.\n");
-    printf("  (7) Determinantes.\n");
-    printf("  (8) Voltar ao Menu Principal.\n");
-    printf("  (9) Sair.\n");
-    printf("      Qual operacao deseja executar? ");
-    scanf("%d", &opc_smenu_matriz);
-
-    switch (opc_smenu_matriz) 
-	{
-        case 1:
-            printf("\n");
-            break;
-        case 2:
-            printf("\n");
-            break;
-        case 3:
-            printf("\n");
-            break;
-        case 4:
-            printf("\n");
-            break;
-        case 5:
-            printf("\n");
-            break;
-        case 6:
-            printf("\n");
-            break;
-        case 7:
-            printf("\n");
-            break;
-        case 8:
-            menu();
-            break;
-        case 9:
-            printf("      Obrigado por utilizar este programa!\n");
-            break;
-        default:
-            printf("      Opcao invalida. Digite outra opcao.\n");
-            smenu_matriz();
-    }
-}
-
-//=== Menu Principal ===// FEITO POR SILVEIRAX
-
-int menu () 
-{
-
-    // Area de Declaracao de Variaveis:
-
-    int opc_menu;
-
-    // Area do Algoritmo:
-
-    printf("==================================================\n");
-    printf("||       CALCULADORA VETORIAL E MATRICIAL       ||\n");
-    printf("==================================================\n");
-    printf("  (1) Operacoes Vetoriais.\n");
-    printf("  (2) Operacoes Matriciais.\n");
-    printf("  (3) Sair.\n");
-    printf("      Qual operacao deseja executar? ");
-    scanf("%d", &opc_menu);
-
-    switch(opc_menu) 
-	{
-        case 1:
-            smenu_vetor();
-            break;
-        case 2:
-            smenu_matriz();
-            break;
-        case 3:
-            printf("      Obrigado por utilizar este programa!\n");
-            break;
-        default:
-            printf("      Opcao invalida. Digite outra opcao.\n");
-            menu();
-    }
+	switch (opc_smenu_vetor){ //=== Gambiarra para fazer tudo numa funcao so ===// FEITO POR DasGatas o rei das gambiarras
+		case 1:
+			Swi=0; // Arrumar essa gambiarra o quanto antes, so ta lendo um vetor em vez de ler N //
+			while(Swi<len)
+			{
+			LerVet(V[Swi].Vet, k);
+			Swi++;
+			}
+			break;
+		case 2:
+			SomaVet(V[0].Vet, V[1].Vet, k);
+			break;
+	}
 }
