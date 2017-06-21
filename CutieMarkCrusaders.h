684c88d83095484ff4dcf3122d9a00ab5f21ead4
Júnior <Babilonia364@users.noreply.github.com>
2017-06-21 19:33:09 -0300

    Update CutieMarkCrusaders.h

--- CutieMarkCrusaders.h
+++ CutieMarkCrusaders.h
@@ -3,85 +3,94 @@
 //Cabecalho com funcoes//
 void menu();
 												// --------- FUNCOES DOS VETORES --------- //
-int* SomaVet(int *V1, int *V2, int *n, int S);
+float* SomaVet(float *V1, float *V2, int n, int n2, int S);
 void DeclaraVet(int opc_smenu_vetor);
-int LerVet(int *V1, int *L, int S);
-int LerMat(int **M, int *Lin, int *Col, int Matz);
+int LerVet(float *V1, int L);
+int LerMat(float *M, int Lin, int Col, int Matz);
 												// --------- FUNCOES DAS MATRIZES --------- //
 void DeclaraMat(int opc_smenu_matriz);
 												// --------- ESCOPO DAS FUNCOES VETORES ---------//
 
 struct Vetores 															//Struct para definir N vetores//
 {
-	int *Vet;
+	float *Vet;
+	int linha;
 };
 
 struct Matrizes 														//Struct para definir N Matrizes//
 {
-	int **Mat;
+	float **Mat;
+	int linha, coluna;
 };
 
 //=== Ler Vetores ===// FEITO POR Twily
 
-int LerVet(int *V1, int *L, int S) 										//Passa 2 vetores para a funcao o vetor a ser lido e seu tamanho//
+int LerVet(float *V1, int L) 											//Passa 1 vetor e 1 inteiro para a funcao o vetor a ser lido e seu tamanho//
 {
 	int m;
 	printf("| ");
-	for (m = 0; m<L[S]; m++) 											//Le o vetor e printa na tela//
+	for (m = 0; m<L; m++) 												//Le o vetor e printa na tela//
 	{
-		printf("%d ", V1[m]);
+		printf("%f ", V1[m]);
 	}
 	printf("|\n\n");
 }
 
 //=== Soma Vetorial ===// FEITO POR Twily
 
-int* SomaVet(int *V1, int *V2, int *n, int S) 							//Passa 2 vetores e seu tamanho na tela//
+float* SomaVet(float *V1, float *V2, int n, int n2, int S) 								//Passa 2 vetores e seu tamanho na tela//
 {
-	int *Soma2=malloc(n[S]*sizeof(int)), m;
-	for(m=0; m<n[S]; m++) 
+	int m;
+	for(m=0; m<n; m++) 
 	{
-		Soma2[m]=0;
-		Soma2[m] = V1[m]; 												//Atribui os valores do vetor 1-n a Soma2//
 		if(S>=1)
 		{
-			if(V2[m]>1000)
+			if(n==n2) 													//Faz a soma caso haja 2 vetores e eles sejam de tamanhos iguais//
 			{
-			V2[m]=0;
-			Soma2[m]=Soma2[m]+V2[m]; 									//Faz a soma caso haja 2 vetores//
-			printf("%d ", Soma2[m]);
-			} else{Soma2[m]=Soma2[m]+V2[m]; printf("%d ", Soma2[m]);}
+				V2[m]=V1[m]+V2[m];
+				printf("%f ", V2[m]);
+			} else
+			{								
+				printf("Vetores de tamanhos diferentes, operacao invalida");
+			}
 		}
 	}
 	printf("\n\n");
-	return Soma2;
+	return V2;
 }
 
 //=== Declaração de vetores ===// FEITO POR Twily
 
 void DeclaraVet(int opc_smenu_vetor)
 {
-	int *Soma=malloc(100*sizeof(int));
-	int *k=malloc(100*sizeof(int)), len, i=0,j=0; 						//Declara um vetor 'k[x]' para o tamanho dos vetores, quantos vetores ele quer 'len' e uma variaveis de controle i e j//
+	int len, i=0,j=0; 													//Declara quantos vetores ele quer 'len' e uma variaveis de controle i e j//
 	
 	printf("Digite quantos vetores voce quer operar:\n");
 	scanf("%d", &len);
 	
 	struct Vetores V[len]; 												//Vetorizando o struct, criando quantos vetores o Leopardo desejar//
-	k[len]; 															//Definindo quantos tamanhos será armazeno nesse vetor malloqueiro//
+	struct Vetores Soma;
 	
 	for (i=0; i<len; i++)
 	{
-	printf("Digite o tamanho do vetor %d\n", i+1);
-	scanf("%d", &k[i]);
+		printf("Digite o tamanho do vetor %d\n", i+1);
+		scanf("%d", &V[i].linha);
+		if(i>=1)														//Se for a segunda vez que o laco roda
+		{
+			if(V[i].linha>=V[i-1].linha)
+			{
+				Soma.linha=V[i].linha;
+			} else {Soma.linha=V[i-1].linha;}
+		}
 	}
+	Soma.Vet=malloc(Soma.linha*sizeof(float));							//Declarando o vetor que armazenara a soma
 	for(j=0; j<len; j++)
 	{
-		V[j].Vet = malloc(k[j]*sizeof(int));
-		for(i=0; i<k[j]; i++)
+		V[j].Vet = malloc(V[j].linha*sizeof(float));
+		for(i=0; i<V[j].linha; i++)
 		{
 			printf("Digite o elemento %d do vetor %d: ", i+1, j+1);
-			scanf("%d", &V[j].Vet[i]);
+			scanf("%f", &V[j].Vet[i]);
 			printf("\n");
 		}
 	}
@@ -93,7 +102,7 @@ void DeclaraVet(int opc_smenu_vetor)
 			j=0;
 			while(j<len)
 			{
-			LerVet(V[j].Vet, k, j); 									//Puxa a funcao ler vetores e printa - los na tela//
+			LerVet(V[j].Vet, V[j].linha); 									//Puxa a funcao ler vetores e printa - los na tela//
 			j++;
 			}
 			menu();
@@ -104,7 +113,7 @@ void DeclaraVet(int opc_smenu_vetor)
 				j=0;
 				while(j<len)
 				{
-					Soma = SomaVet(V[j].Vet, Soma, k, j);				//Puxa a função soma
+					Soma.Vet = SomaVet(V[j].Vet, Soma.Vet, V[j].linha, Soma.linha, j);				//Puxa a função soma
 					j++;
 				}
 			} else {printf(":)\n:)\n:)\n");}
@@ -123,30 +132,28 @@ void DeclaraVet(int opc_smenu_vetor)
 void DeclaraMat(int opc_smenu_matriz)
 {
 	int matz, i, j, k;																	//Declarando a variavel de quantidade de matriz 'matz' e possiveis contadores
-	int *lin=malloc(100*sizeof(int));													//Declarando vetores para armazenar tamanhos de linhas e colunas
-	int *col=malloc(100*sizeof(int));
-	
 	printf("  Digite quantas matrizes voce deseja operar: ");
 	scanf("%d", &matz);
 	struct Matrizes M[matz];
 	
 	for(i=0; i<matz; i++)
 	{
-		printf("\n  Digite o numero de linhas e colunas de sua matriz %d: ", i+1);
-		scanf("%d %d", &lin[i], &col[i]);
+		printf("\n  Digite o numero de linhas de sua matriz %d: ", i+1);
+		scanf("%d", &M[i].linha);
+		printf("\n  Digite o numero de colunas de sua matriz %d: ", i+1);
+		scanf("%d", &M[i].coluna);
 		printf("\n");
 	}
-	for (j=0; j<matz; j++)																//Alocacao dinamica de linhas e colunas
+	for (k=0; k<matz; k++)																//Alocacao dinamica de linhas e colunas
 	{
-		M[j].Mat=malloc(col[j]*sizeof(int));
-		for(i=0; i<col[j]; i++)
+		M[k].Mat=malloc(M[k].coluna*sizeof(float));
+		for(i=0; i<M[k].coluna; i++)
 		{
-			M[j].Mat[i]=malloc(lin[i]*sizeof(int));
-			for(k=0; k<lin[j]; k++)														//Definindo elementos da matriz
+			M[k].Mat[i]=malloc(M[k].linha*sizeof(float));
+			for(j=0; j<M[k].linha; j++)														//Definindo elementos da matriz
 			{
-				printf("  Digite o elemento da linha %d e coluna %d da matriz %d: ", k+1, i+1, j+1);
-				scanf("%d", &M[j].Mat[i][k]);
-				printf("\n");
+				printf("  Digite o elemento da linha %d e coluna %d da matriz %d: ", i+1, j+1, k+1);
+				scanf("%f", &M[k].Mat[i][j]);
 			}
 		}
 	}
@@ -156,7 +163,7 @@ void DeclaraMat(int opc_smenu_matriz)
 			i=0;
 			while(i<matz)
 			{
-				LerMat(M[i].Mat, lin, col, i);											//Puxando a função ler matrizes
+				LerMat(&M[i].Mat[0][0], M[i].linha, M[i].coluna, i);											//Puxando a funcao ler matrizes
 				i++;
 			}
 			menu();
@@ -169,15 +176,15 @@ void DeclaraMat(int opc_smenu_matriz)
 	}
 }
 
-int LerMat(int **M, int *Lin, int *Col, int Matz)
+int LerMat(float *M, int Lin, int Col, int Matz)
 {
 	int ler1, ler2;
-	for (ler1=0; ler1<Col[Matz]; ler1++)
+	for (ler1=0; ler1<Col; ler1++)
 	{
 		printf("|  ");
-		for (ler2=0; ler2<Lin[Matz]; ler2++)
+		for (ler2=0; ler2<Lin; ler2++)
 		{
-			printf("%d  ", M[Col[ler1]][Lin[ler2]]);
+			printf("%f  ", *(M+(ler2*1)+ler1));
 		}
 		printf(" |\n");
 	}
