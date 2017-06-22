1c94f0f78e0c55801a99b6472e9aa14ba4f86c11
Alison Silveira <silveira.jam@gmail.com>
2017-06-22 19:42:13 -0300

    Update CutieMarkCrusaders.h
    Biblioteca renovada...

--- CutieMarkCrusaders.h
+++ CutieMarkCrusaders.h
@@ -57,73 +57,51 @@ struct Matrizes {
 
 // (1.1) Inserir Vetores FEITO POR Twily
 
-//=== Declaração de vetores ===// FEITO POR Twily
+void InsVet(int opc_smenu_vet) {
 
-void InsVet(int opc_smenu_vet)
-{
-	int len, i=0,j=0; 													//Declara quantos vetores ele quer 'len' e uma variaveis de controle i e j//
+    // Declarar quantos vetores se quer 'len' e variaveis de controle i e j.
 
-	printf("Digite quantos vetores voce quer operar:\n");
+	int len, i = 0, j = 0;
+
+	printf("  Insira o numero de vetores a se operar: ");
 	scanf("%d", &len);
+	printf("\n");
 
-	struct Vetores V[len]; 												//Vetorizando o struct, criando quantos vetores o Leopardo desejar//
+    // Vetorizar o struct e criar quantos vetores se desejar.
+
+	struct Vetores V[len];
 	struct Vetores Soma;
 
-	for (i=0; i<len; i++)
-	{
-		printf("Digite o tamanho do vetor %d\n", i+1);
+	for (i = 0; i < len; i++) {
+		printf("  Insira o numero de coordenadas do vetor %d: ", i + 1);
 		scanf("%d", &V[i].linha);
-		if(i>=1)														//Se for a segunda vez que o laco roda
-		{
-			if(V[i].linha>=V[i-1].linha)
-			{
-				Soma.linha=V[i].linha;
-			} else {Soma.linha=V[i-1].linha;}
-		}
-	}
 
-	Soma.Vet=malloc(Soma.linha*sizeof(float));							//Declarando o vetor que armazenara a soma
-	for(j=0; j<len; j++)
-	{
-		V[j].Vet = malloc(V[j].linha*sizeof(float));
-		for(i=0; i<V[j].linha; i++)
-		{
-			printf("Digite o elemento %d do vetor %d: ", i+1, j+1);
-			scanf("%f", &V[j].Vet[i]);
-			printf("\n");
-		}
-	}
+    // Se for a segunda vez que o laco roda
 
-	//=== Switch para puxar as outras funcoes presentes na biblio ===// FEITO POR Twily
-
-	switch (opc_smenu_vet){
-		case 1:
-			j=0;
-			while(j<len)
-			{
-			ExiVet(V[j].Vet, V[j].linha); 									//Puxa a funcao ler vetores e printa - los na tela//
-			j++;
+		if(i >= 1) {
+			if (V[i].linha>=V[i-1].linha) {
+                Soma.linha=V[i].linha;
 			}
-			break;
-		case 2:
-			if(len>=2)
-			{
-				j=0;
-				while(j<len)
-				{
-					Soma.Vet = SomaVet(V[j].Vet, Soma.Vet, V[j].linha, Soma.linha, j);				//Puxa a função soma
-					j++;
-				}
-			} else {printf(":)\n:)\n:)\n");}
-			menu();
-			break;
-		default:
-			printf("      Opcao invalida. Digite outra opcao.\n");
-			break;
+			else {
+                Soma.linha=V[i-1].linha;
+            }
+		}
 	}
-}
 
-// (1.2) Exibir Vetores Inseridos
+	Soma.Vet = malloc(Soma.linha * sizeof(float));  // Declarando o vetor que armazenara a soma
+
+    printf("\n");
+	for (j = 0; j < len; j++) {
+		V[j].Vet = malloc(V[j].linha * sizeof(float));
+		for(i = 0; i < V[j].linha; i++) {
+			printf("  Insira o elemento %d do vetor %d: ", i + 1, j + 1);
+			scanf("%f", &V[j].Vet[i]);
+		}
+	}
+	printf("\n");
+
+
+// (1.2) Exibir Vetores Inseridos:
 
     // Passa 1 vetor e 1 inteiro para a funcao o vetor a ser lido e seu tamanho.
 
@@ -131,34 +109,36 @@ void ExiVet(float *V1, int L) {
 
 	int m;
 
-    // Le o vetor e printa na tela.
+    // Le o vetor e imprime na tela:
 
 	printf("  ( ");
 	for (m = 0; m < L; m++) {
 		printf("%.1f ", V1[m]);
 	}
-	printf(")\n\n");
+	printf(")\n \n");
 }
 
+// Soma Vetorial FEITO POR Twily:
 
-//=== Soma Vetorial ===// FEITO POR Twily
+    // Passa 2 vetores e seu tamanho na tela:
 
-float* SomaVet(float *V1, float *V2, int n, int n2, int S) 								//Passa 2 vetores e seu tamanho na tela//
-{
-	float *Soma2=malloc(n*sizeof(float));
+float* SomaVet(float *V1, float *V2, int n, int n2, int S) {
+
+	float *Soma2 = malloc(n * sizeof(float));
 	int m;
-	for(m=0; m<n; m++)
-	{
-		Soma2[m]=V1[m];
-		if(S>=1)
-		{
-			if(n==n2) 													//Faz a soma caso haja 2 vetores e eles sejam de tamanhos iguais//
-			{
-				V2[m]=Soma2[m]+V2[m];
+
+	for (m = 0; m < n; m++) {
+		Soma2[m] = V1[m];
+		if(S >= 1) {
+
+			// Faz a soma caso haja 2 vetores e eles sejam de tamanhos iguais
+
+			if (n == n2) {
+				V2[m] = Soma2[m] + V2[m];
 				printf("%f ", V2[m]);
-			} else
-			{
-				printf("Vetores de tamanhos diferentes, operacao invalida");
+			}
+			else {
+				printf("\7      Vetores de tamanho diferentes. Digite outra opcao.\n \n");
 			}
 		}
 	}
@@ -167,14 +147,46 @@ float* SomaVet(float *V1, float *V2, int n, int n2, int S) 								//Passa 2 vet
 	return V2;
 }
 
+
+// SWITCH PARA PUXAR FUNÇÕES DA BIBLIOTECA --- FEITO POR Twily:
+
+	switch (opc_smenu_vet){
+		case 1:
+			j = 0;
+			while (j < len) {             // Puxa a funcao ler vetores e printa-los na tela
+			ExiVet(V[j].Vet, V[j].linha);
+			j++;
+			}
+			break;
+		case 2:
+		    break;
+		case 3:
+			if (len >= 2) {
+				j = 0;
+				while(j < len) {
+					Soma.Vet = SomaVet(V[j].Vet, Soma.Vet, V[j].linha, Soma.linha, j);				// Puxa a função soma
+					j++;
+				}
+			} else {printf(":)\n:)\n:)\n");}
+			break;
+		default:
+			printf("\7      Opcao invalida. Digite outra opcao.\n \n");
+			break;
+	}
+}
+
+
 /* ESCOPO DAS FUNCOES COM MATRIZES */
 
 // Declarando as Matrizes FEITO POR Twily:
 
 void InsMat(int opc_smenu_mat) {            // Declarando a variavel de quantidade de matriz 'matz' e possiveis contadores
+
 	int matz, i, j, k;
+
 	printf("  Digite quantas matrizes voce deseja operar: ");
 	scanf("%d", &matz);
+
 	struct Matrizes M[matz];
 
 	for (i = 0; i < matz; i++) {
